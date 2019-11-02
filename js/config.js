248f68ad5ef40c7b92f2e5abc88edb5ea9e9322f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-03 00:52:11 +0900

    🤩(coding): rm

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCMsVRs6KS0r9foHAN3hMimERlqgbMoJLA",
+    authDomain: "joshi-ana.firebaseapp.com",
+    databaseURL: "https://joshi-ana.firebaseio.com",
+    projectId: "joshi-ana",
+    storageBucket: "joshi-ana.appspot.com",
+    messagingSenderId: "842661780287",
+    appId: "1:842661780287:web:d3ee2f7a57ec9d7351118a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

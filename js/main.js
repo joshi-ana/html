768ec7d052f2d840bc97880f92deb25c2366ab51
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-03 08:02:08 +0900

    🤩(coding): window -standalone

--- js/main.js
+++ js/main.js
@@ -10,8 +10,13 @@ document.addEventListener('deviceready', function () {
   navigator.splashscreen.hide();
 }, false);
 
-// if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
-//   // <html> 要素に属性を追加（値として空文字列を設定）
-//   document.documentElement.setAttribute('onsflag-iphonex-portrait', '') // 縦
-//   document.documentElement.setAttribute('onsflag-iphonex-landscape', '') // 横
-// }
+if (window.navigator.standalone) {
+  if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
+    // <html> 要素に属性を追加（値として空文字列を設定）
+    document.documentElement.setAttribute('onsflag-iphonex-portrait', '') // 縦
+    document.documentElement.setAttribute('onsflag-iphonex-landscape', '') // 横
+  }
+
+} else {
+  // document.write("このページをホームに追加してください");
+}
